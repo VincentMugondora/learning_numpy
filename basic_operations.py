ec467741b697f57523111197218249d3822503ad
vincentmugondora <vinmugondora@gmail.com>
2025-04-16 13:40:50 +0200

    starting strings in numpy

--- basic_operations.py
+++ basic_operations.py
@@ -15,3 +15,16 @@ print(a.shape)
 
 a = np.array([[1,2], [3,4], [5,6]], dtype=np.complex64)
 print(a)
+
+b = np.zeros((3, 4))  # create a 3x4 array of zeros
+print(b)
+
+b = np.ones((3, 4))  # create a 3x4 array of ones
+print(b)
+
+l = range(5)
+print(l)
+
+l = np.arange(5)  # create an array of numbers from 0 to 4
+print(l)
+

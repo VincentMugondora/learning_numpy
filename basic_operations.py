68e67228b318c8af229aadec42c643578bf32a96
vincentmugondora <vinmugondora@gmail.com>
2025-04-16 12:34:01 +0200

    created an array and used number of dimensions and itemsize

---
+++ basic_operations.py
@@ -0,0 +1,7 @@
+# numpy basic operations
+import numpy as np
+
+a = np.array([[1,2], [3,4], [5,6]])
+print(a)
+print(a.ndim)  # number of dimensions
+print(a.itemsize)  # size of each element in bytes

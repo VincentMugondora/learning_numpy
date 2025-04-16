1d010584a246c6226d674cad1d02bf14df11fcd1
vincentmugondora <vinmugondora@gmail.com>
2025-04-16 11:41:59 +0200

    starting numpy

---
+++ main.py
@@ -0,0 +1,8 @@
+# numpy => is the core library for scientific and numerical computing in python
+# it provides high-peformance multidinmensinal array object and tools for working with arrays
+
+import numpy as np
+
+# create a numpy array
+a = np.array([1, 2, 3, 4, 5])
+print(a)  # [1 2 3 4 5]

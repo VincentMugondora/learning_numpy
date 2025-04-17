5b116718c73f0e25b4fb44b14701608a86d13f81
vincentmugondora <vinmugondora@gmail.com>
2025-04-17 10:43:08 +0200

    feat: add resizing functionality for numpy arrays in resizing_array.py

---
+++ resizing_array.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# resizing arrays
+a = np.array([[1, 2, 3], [4, 5, 6]])
+print(f'original array:\n{a}')
+
+print(f'The shape of the array is: {a.shape}') # shape of the array
+
+b = np.resize(a, (3, 2)) # resize the array to 3 rows and 2 columns
+print(f'\nresized array:\n{b}')
+print(f'The shape of the resized array is: {b.shape}') # shape of the resized array 
+
+b = np.resize(a, (2, 3)) # resize the array to 2 rows and 3 columns
+print(f'\nresized array:\n{b}')
+print(f'The shape of the resized array is: {b.shape}')
